3b58d1bcc79464a2277d7f288033fbd8b900358a
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-04-28 13:58:38 +0300

    Add validation in model

--- HW3/Models/Entities/Friend.cs
+++ HW3/Models/Entities/Friend.cs
@@ -1,10 +1,21 @@
-﻿namespace HW4.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HW4.Models.Entities
 {
     public class Friend
     {
+       
+        [Required]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле не может быть пустым")]
         public string FriendName { get; set; }
+
+        [MaxLength(25)]
         public string Place { get; set; }
+
+        [Range(0, 200)]
         public int FriendAge { get; set; }
     }
 }
+
